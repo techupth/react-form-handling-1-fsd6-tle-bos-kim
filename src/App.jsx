a500ddb8a3fef860e6506c03c4b23554269eb4f4
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-06-05 11:44:05 +0700

    feat: add event update greeting text

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+  const [newGreeting, setNewGreeting] = useState("");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={newGreeting}
+          onChange={(event) => {
+            setNewGreeting(event.target.value);
+          }}
+        />
       </div>
-
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setGreeting(newGreeting);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
