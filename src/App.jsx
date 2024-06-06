a12900d955cc0c712caa73721b110696f4fec6b1
tle <tlemakuz@gmail.com>
2024-06-06 18:13:09 +0700

    feat: handle1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const firstGreeting = "greeting Message";
+  const [isGreeting, setGreeting] = useState(firstGreeting);
+  
+  const [getText, setText] = useState("");
+  
+  const updateText = () => {
+    setGreeting(getText);
+    setTimeout(() => {setGreeting(firstGreeting),setText("");}, 1500);
+
+  }
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{isGreeting}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" value={getText} onChange={(event) => setText(event.target.value)}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateText}>Update text</button>
       </div>
     </div>
   );
